1d1f61287921fe0856ed10b2d0b985f6d0153e15
Lemonade-up <wmaglangit1118@gmail.com>
2021-12-09 21:38:57 +0800

    Initial Commit, Pseudo Code added

---
+++ Program no.2.py
@@ -0,0 +1,13 @@
+# Program 2: Guess the number
+# Generate 1 random number (0-100)
+# Ask the user to guess the number
+# Display “Greater than” if the inputed number is greater than the random number
+# Display “Less than” if the inputed number is less than the random number
+# Repeat asking the user until the random number has been guessed correctly.
+
+# CONDITIONAL LOOP
+
+# 1 Generate Random Number 0-100
+# 3 Conditional Loop
+# 4 Display if Guess is greater or lesser
+# 5 Print Congratutalions 
